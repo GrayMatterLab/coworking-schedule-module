408910199400ec469e397158f1b0b6019b3fa19a
Moe Chowdhury <moe.chowdhury@outlook.com>
2020-11-23 14:26:17 -0500

    Installed all dependencies git status

---
+++ client/src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
