52457581253df24a508a545baa55178ce37f13d4
Moe Chowdhury <moe.chowdhury@outlook.com>
2020-11-23 20:24:08 -0500

    Created a MongoDB using Mongoose plus Schema

---
+++ database/index.js
@@ -0,0 +1,29 @@
+//Open connection to the space database on our locally running instance of MongoDB
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/space', { useNewURLParser: true });
+
+//Notification if database is connected successfully or if a connection error occurs
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function () {
+  console.log('We\'re connected!')
+});
+
+//Schema
+import mongoose from 'mongoose';
+
+const reservationSchema = new mongoose.Schema(
+  {
+    location: { type: String, required: true },
+    pricePerHour: { type: Number, required: true },
+    reserveDate: { type: Date, required: true },
+    checkInTime: { startTime: Date, required: true },
+    checkOutTime: { endtime: Date, required: true },
+    numberOfGuests: { type: Number, required: true },
+    occupancyTaxesAndFees: { type: Number, required: true }
+  }
+)
+
+const Reservation = mongoose.model('Reservation', reservationSchema);
+
+export default Reservation;
