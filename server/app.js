a27e3a6b1e5d96a97424aee28599b51da68d9c27
Moe Chowdhury <moe.chowdhury@outlook.com>
2020-11-23 19:49:09 -0500

    Implemented test API calls

--- server/app.js
+++ server/app.js
@@ -5,3 +5,16 @@ const app = express();
 app.use(express.static(__dirname + '/../client/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//Test API GET request
+app.get('/', (req, res) => {
+  res.send('Received successful GET test request!')
+})
+
+//Test API POST request
+app.post('/', (req, res) => {
+  res.send('Received successful POST test request!')
+})
+
+
+module.exports = app;
