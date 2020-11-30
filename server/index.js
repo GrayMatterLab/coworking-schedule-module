a757885d2fff2f23e99c07fe4599d55833bed794
Moe Chowdhury <moe.chowdhury@outlook.com>
2020-11-30 17:31:15 -0500

    Removed a bunch of code that doesn't work

--- server/index.js
+++ server/index.js
@@ -1,14 +1,7 @@
 const app = require('./app.js');
-// const mongoose = require('mongoose');
 
 const port = 3000;
 
-//Connect to MongoDB database
-// mongoose
-//   .connect("mongodb://localhost:27017/space", { useNewUrlParser: true })
-//   .then(() => {
-//Listening to port
 app.listen(port, () => {
   console.log(`Server has started on port: ${port}!`)
 })
-  // })
