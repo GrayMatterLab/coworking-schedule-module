249b8419c77c159ebb91241a33a5a53487a6547a
Moe Chowdhury <moe.chowdhury@outlook.com>
2021-01-20 21:54:28 -0500

    Resolved CSS error

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,55 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import * as s from './styled-components.js';
+import Search from './Search.js';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      
-    };
-  }
 
-  componentDidMount(){
-    const selected = document.querySelector(".selected");
-    const optionsContainer = document.querySelector(".options-container");
-
-    const optionsList = document.querySelectorAll(".option");
-
-    selected.addEventListener("click", () => {
-      optionsContainer.classList.toggle("active");
-      console.log('Time In Box Selected')
-    });
-
-    optionsList.forEach(o => {
-      o.addEventListener("click", () => {
-        console.log('Time In dropdown was triggered')
-        selected.innerHTML = o.querySelector("label").innerHTML;
-        optionsContainer.classList.remove("active");
+function App(){
+    window.onload=function(){
+      const selected = document.querySelector(".selected");
+      const optionsContainer = document.querySelector(".options-container");
+    
+      const optionsList = document.querySelectorAll(".option");
+    
+      selected.addEventListener("click", () => {
+        optionsContainer.classList.toggle("active");
+        console.log('Time In Box Selected')
       });
-    });
-
-    const selectedq = document.querySelector(".selectedq");
-    const optionsContainerq = document.querySelector(".options-containerq");
-
-    const optionsListq = document.querySelectorAll(".optionq");
-
-    selectedq.addEventListener("click", () => {
-      optionsContainerq.classList.toggle("active");
-      console.log('Time Out Box Selected')
-    });
-
-    optionsListq.forEach(e => {
-      e.addEventListener("click", () => {
-        console.log('Time Out dropdown was triggered')
-        selectedq.innerHTML = e.querySelector("label").innerHTML;
-        optionsContainerq.classList.remove("active");
+    
+      optionsList.forEach(o => {
+        o.addEventListener("click", () => {
+          console.log('Time In dropdown was triggered')
+          selected.innerHTML = o.querySelector("label").innerHTML;
+          optionsContainer.classList.remove("active");
+        });
       });
-    });
-  }
+    
+      const selectedq = document.querySelector(".selectedq");
+      const optionsContainerq = document.querySelector(".options-containerq");
+    
+      const optionsListq = document.querySelectorAll(".optionq");
+    
+      selectedq.addEventListener("click", () => {
+        optionsContainerq.classList.toggle("active");
+        console.log('Time Out Box Selected')
+      });
+    
+      optionsListq.forEach(e => {
+        e.addEventListener("click", () => {
+          console.log('Time Out dropdown was triggered')
+          selectedq.innerHTML = e.querySelector("label").innerHTML;
+          optionsContainerq.classList.remove("active");
+        });
+      });    
+    }
   
-  render() {
+  const [showSearch, setShowSearch] = useState(false);
+
+    
     return (
       <s.Grid>
         <s.PricePerNight>
@@ -60,11 +57,13 @@ export default class App extends React.Component {
         </s.Rating>
         <s.CheckInContainer>
           <s.CheckInDate>
-            Date
+            {/* <div classname="banner_search">
+              {showSearch && <Search />}
+              <button onClick={()=> setShowSearch(!showSearch)} 
+              class='banner_searchButton' variant='outlined'>Search Dates</button>
+            </div> */}
+            <Search />
           </s.CheckInDate>
-          <s.Attendee>
-            1 Guest
-          </s.Attendee>
           <s.TimeIn>
           <div class="select-box">
             <div class="options-container">
@@ -129,6 +128,5 @@ export default class App extends React.Component {
         </s.CostBreakdown>
       </s.Grid >
     )
-  }
 }
 ReactDOM.render(<App />, document.getElementById('app'));
