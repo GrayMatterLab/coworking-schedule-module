b8924d85239e6cdb09671afacea52c4baa7b2d49
Moe Chowdhury <moe.chowdhury@outlook.com>
2021-01-23 15:49:15 -0500

    Refactored TimeOut component

---
+++ client/src/TimeOut.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function TimeOut(){
+    return (
+        <div class="select-boxq">
+            <div class="options-containerq">
+              <div class="optionq">
+                <input type="radio" class="radioq" />
+                <label for="film">9:00 AM</label>
+              </div>
+            </div>
+            <div class="selectedq">
+              Check Out
+            </div>
+        </div>
+    )
+}
+
+export default TimeOut;
