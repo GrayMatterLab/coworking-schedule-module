837395d623604728c24fc5d56408c6cad072939d
Moe Chowdhury <moe.chowdhury@outlook.com>
2021-01-23 15:45:13 -0500

    Refactored Check-In Time Component

---
+++ client/src/TimeIn.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function TimeIn(){
+    return(
+        <div class="select-box">
+            <div class="options-container">
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">Film & Animation</label>
+              </div>
+            </div>
+            <div class="selected">
+              Check In
+            </div>
+        </div>
+    )
+}
+
+export default TimeIn;
