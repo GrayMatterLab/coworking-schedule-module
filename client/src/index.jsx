9152b43f7eddbb0f673a5e68897b6afbbecf755d
Moe Chowdhury <moe.chowdhury@outlook.com>
2021-01-23 23:05:04 -0500

    Refactored Toggle Action into its own seperate .js file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,49 +4,10 @@ import * as s from '../public/styled-components.js';
 import Search from './Search.js';
 import TimeIn from './TimeIn.js';
 import TimeOut from './TimeOut.js';
-
+import ToggleAction from './ToggleAction.js'
 
 function App(){
-    window.onload=function(){
-      const selected = document.querySelector(".selected");
-      const optionsContainer = document.querySelector(".options-container");
-    
-      const optionsList = document.querySelectorAll(".option");
-    
-      selected.addEventListener("click", () => {
-        optionsContainer.classList.toggle("active");
-        console.log('Time In Box Selected')
-      });
-    
-      optionsList.forEach(o => {
-        o.addEventListener("click", () => {
-          console.log('Time In dropdown was triggered')
-          selected.innerHTML = o.querySelector("label").innerHTML;
-          optionsContainer.classList.remove("active");
-        });
-      });
-    
-      const selectedq = document.querySelector(".selectedq");
-      const optionsContainerq = document.querySelector(".options-containerq");
-    
-      const optionsListq = document.querySelectorAll(".optionq");
-    
-      selectedq.addEventListener("click", () => {
-        optionsContainerq.classList.toggle("active");
-        console.log('Time Out Box Selected')
-      });
-    
-      optionsListq.forEach(e => {
-        e.addEventListener("click", () => {
-          console.log('Time Out dropdown was triggered')
-          selectedq.innerHTML = e.querySelector("label").innerHTML;
-          optionsContainerq.classList.remove("active");
-        });
-      });    
-    }
-  
-  const [showSearch, setShowSearch] = useState(false);
-
+  ToggleAction();
     
     return (
       <s.Grid>
