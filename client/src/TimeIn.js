55ffe5ff8b30791de6132cd19c611cc8d6d9c7cd
Moe Chowdhury <moe.chowdhury@outlook.com>
2021-01-25 17:16:40 -0500

    Checkin Time Completed

--- client/src/TimeIn.js
+++ client/src/TimeIn.js
@@ -6,7 +6,99 @@ function TimeIn(){
             <div class="options-container">
               <div class="option">
                 <input type="radio" class="radio" />
-                <label for="film">Film & Animation</label>
+                <label for="film">1:00 AM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">2:00 AM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">3:00 AM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">4:00 AM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">5:00 AM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">6:00 AM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">7:00 AM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">8:00 AM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">9:00 AM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">10:00 AM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">11:00 AM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">12:00 PM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">1:00 PM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">2:00 PM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">3:00 PM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">4:00 PM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">5:00 PM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">6:00 PM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">7:00 PM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">8:00 PM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">9:00 PM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">10:00 PM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">11:00 PM</label>
+              </div>
+              <div class="option">
+                <input type="radio" class="radio" />
+                <label for="film">12:00 PM</label>
               </div>
             </div>
             <div class="selected">
