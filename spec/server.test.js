91ea86dc74631ae60936fd5c2f7c393dc26cff7a
Moe Chowdhury <moe.chowdhury@outlook.com>
2021-02-16 14:45:26 -0500

    Added api locations and reservation routes to test

--- spec/server.test.js
+++ spec/server.test.js
@@ -2,22 +2,37 @@
 const app = require("../server/app"); // Link to your server file
 const supertest = require("supertest");
 const request = supertest(app);
-
 import 'regenerator-runtime/runtime'
 
-it("Checking to see if Jest works.", () => {
-    expect(1).toBe(1);
+describe('Jest', () => {
+    it("Checking to see if Jest works.", () => {
+        expect(1).toBe(1);
+    });
 });
 
-it("GET request to Express Server '/' Endpoint", async done => {
-    const response = await request.get("/");
-    expect(response.status).toBe(200);
-    done();
-});
+describe("Testing Express Server Routes", () => {
+    it("GET request to '/' endpoint", async done => {
+        const response = await request.get("/");
+        expect(response.status).toBe(200);
+        done();
+    });
 
-it("POST request to Express Server '/' Endpoint", async done => {
-    const response = await request.post("/");
-    expect(response.status).toBe(200);
-    expect(response.body.message).toBe('Received successful POST test request!')
-    done();
+    it("POST request to '/' endpoint", async done => {
+        const response = await request.post("/");
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('Received successful POST test request!')
+        done();
+    });
+
+    it("GET request to '/api/locations' endpoint", async done => {
+        const response = await request.get("/api/locations");
+        expect(response.status).toBe(200);
+        done();
+    });
+
+    it("POST request to '/api/reservation' endpoint", async done => {
+        const response = await request.post("/api/reservation");
+        expect(response.status).toBe(200);
+        done();
+    });
 });
