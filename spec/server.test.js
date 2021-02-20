68ea88944d132fd68b87fe5e7035eaac2b93b581
Moe Chowdhury <moe.chowdhury@outlook.com>
2021-02-20 13:13:16 -0500

    Added test coverage with data types from API endpoint

--- spec/server.test.js
+++ spec/server.test.js
@@ -11,7 +11,7 @@ describe('Jest', () => {
 });
 
 describe("Testing Express Server Status Codes are 200", () => {
-    it("GET request to '/' endpoint", async done => {
+    it("GET request from '/' endpoint", async done => {
         const response = await request.get("/");
         expect(response.status).toBe(200);
         done();
@@ -23,7 +23,7 @@ describe("Testing Express Server Status Codes are 200", () => {
         done();
     });
 
-    it("GET request to '/api/locations' endpoint", async done => {
+    it("GET request from '/api/locations' endpoint", async done => {
         const response = await request.get("/api/locations");
         expect(response.status).toBe(200);
         done();
@@ -34,4 +34,32 @@ describe("Testing Express Server Status Codes are 200", () => {
         expect(response.status).toBe(200);
         done();
     });
+});
+
+describe("Testing all expected data types from '/api/locations' API GET request", () => {
+    it("All the id value is a string in the database", async done => {
+        const response = await request.get("/api/locations");
+        expect(response.body.map(x => typeof(x.pricePerNight))).toStrictEqual(['number'])
+        done();
+    }); 
+    it("All the location value is a string in the database", async done => {
+        const response = await request.get("/api/locations");
+        expect(response.body.map(x => typeof(x.location))).toStrictEqual(['string'])
+        done();
+    }); 
+    it("All the pricePerNight value is a number in the database", async done => {
+        const response = await request.get("/api/locations");
+        expect(response.body.map(x => typeof(x.pricePerNight))).toStrictEqual(['number'])
+        done();
+    }); 
+    it("All the cleaningFee value is a number in the database", async done => {
+        const response = await request.get("/api/locations");
+        expect(response.body.map(x => typeof(x.cleaningFee))).toStrictEqual(['number'])
+        done();
+    }); 
+    it("All the covidSurcharge value is a number in the database", async done => {
+        const response = await request.get("/api/locations");
+        expect(response.body.map(x => typeof(x.covidSurcharge))).toStrictEqual(['number']);
+        done();
+    }); 
 });
