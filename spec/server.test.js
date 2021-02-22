05d8e504ceed773291d52d244c52a4d996937386
Moe Chowdhury <moe.chowdhury@outlook.com>
2021-02-22 14:43:40 -0500

    Added a test for POST req

--- spec/server.test.js
+++ spec/server.test.js
@@ -2,6 +2,7 @@
 const app = require("../server/app"); // Link to your server file
 const supertest = require("supertest");
 const request = supertest(app);
+import { send } from 'process';
 import 'regenerator-runtime/runtime'
 
 describe('Jest', () => {
@@ -13,7 +14,7 @@ describe('Jest', () => {
 describe("Testing Express Server Status Codes are 200", () => {
     it("GET request from '/' endpoint", async done => {
         const response = await request.get("/");
-        expect(response.status).toBe(200);
+        expect(response.statusCode).toBe(200);
         done();
     });
 
@@ -36,7 +37,7 @@ describe("Testing Express Server Status Codes are 200", () => {
     });
 });
 
-describe("Testing all expected data types from '/api/locations' API GET request", () => {
+describe("Testing expected data types from '/api/locations' API GET request", () => {
     it("All the id value is a string in the database", async done => {
         const response = await request.get("/api/locations");
         expect(response.body.map(x => typeof(x.pricePerNight))).toStrictEqual(['number'])
@@ -62,4 +63,49 @@ describe("Testing all expected data types from '/api/locations' API GET request"
         expect(response.body.map(x => typeof(x.covidSurcharge))).toStrictEqual(['number']);
         done();
     }); 
+});
+
+describe("Testing POST request to 'api/reservation'", () => {
+    it("Inserting only 1 set of data into MongoDB", async done => {
+        const mockReservationData = {
+            locations: 'testJestSpot',
+            pricePerNight: 42,
+            cleaningFee: 29,
+            covidSurcharge: 9,
+            occupTaxNFee: 4,
+            timeInSel: '12:22',
+            timeOutSel: '21:33',
+            reserveStartDate: 'testDate',
+            reserveEndDate: 'testDateOut'
+          };
+
+          const response = await request.post("/api/reservation").send(mockReservationData);
+          expect(response.body.insertedCount).toBe(1)
+          expect(response.body.result).toStrictEqual({'n':1, 'ok':1})
+          done();
+    })
+    it("All the fields in the data sent to the API matches with the received data", async done => {
+        const mockReservationData = {
+            locations: 'testJestSpot',
+            pricePerNight: 42,
+            cleaningFee: 29,
+            covidSurcharge: 9,
+            occupTaxNFee: 4,
+            timeInSel: '12:22',
+            timeOutSel: '21:33',
+            reserveStartDate: 'testDate',
+            reserveEndDate: 'testDateOut'
+          };
+
+          const response = await request.post("/api/reservation").send(mockReservationData);
+          expect(response.body.ops[0].locations).toBe(mockReservationData.locations)
+          expect(response.body.ops[0].pricePerNight).toBe(mockReservationData.pricePerNight)
+          expect(response.body.ops[0].cleaningFee).toBe(mockReservationData.cleaningFee)
+          expect(response.body.ops[0].occupTaxNFee).toBe(mockReservationData.occupTaxNFee)
+          expect(response.body.ops[0].timeInSel).toBe(mockReservationData.timeInSel)
+          expect(response.body.ops[0].timeOutSel).toBe(mockReservationData.timeOutSel)
+          expect(response.body.ops[0].reserveStartDate).toBe(mockReservationData.reserveStartDate)
+          expect(response.body.ops[0].reserveEndDate).toBe(mockReservationData.reserveEndDate)
+          done();
+    })
 });
